60377a3438dc4e83f3db368bf4834065598a213e
Karl Keller <kgk86@hotmail.com>
2021-01-07 19:43:00 +0100

    Generate random grid of letters for part 2 - Generating a new game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,10 @@
 class GamesController < ApplicationController
   def new
+    # create @letters storing random letters from the alphabet
+    @letters = []
+    grid_size = 10
+    # Generate random grid of letters
+    @letters = Array.new(grid_size) { ('a'..'z').to_a.sample }
   end
 
   def score
